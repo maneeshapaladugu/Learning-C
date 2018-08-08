ebfe9c61313fa8bcbee2013e0c477c2bccd8581c
maneeshapaladugu <41922387+maneeshapaladugu@users.noreply.github.com>
2018-08-08 22:25:40 +0530

    Update ctrl_stmts.c

--- control statements/ctrl_stmts.c
+++ control statements/ctrl_stmts.c
@@ -2,36 +2,34 @@
 main()
 {
 //positive or negative using control statemets
-/* int n;
- printf("Enter a number\n");
- scanf("%d",&n);
- if(n<=0)
- {
-  if(n==0)
-   printf("You entered zero\n");
+  int n;
+  printf("Enter a number\n");
+  scanf("%d",&n);
+  if(n<=0)
+  {
+     if(n==0)
+       printf("Zero\n");
+     else
+       printf("Negative\n");
+  } 
   else
-   printf("Negative\n");
- }
- else
-  printf("Positive\n");*/
+     printf("Positive\n");*/
 
 //using bitwise
  /*int n,i=sizeof(int)*8-1;
- printf("Enter a number\n");
- scanf("%d",&n);
- if(n==0)
-  printf("Zero\n");
- else if((n&(1<<i))==0)
-  printf("Positive\n");
- else
-  printf("Negative\n");*/
+  printf("Enter a number\n");
+  scanf("%d",&n);
+  if(n==0)
+     printf("Zero\n");
+  else if((n&(1<<i))==0)
+     printf("Positive\n");
+  else
+     printf("Negative\n");*/
 
 //using conditional operator
 /*int n;
-printf("Enter a number\n");
-scanf("%d",&n);
-(n==0)?(n==0)?printf("Zero\n"):printf("Negative\n"):printf("Positve\n");*/
-
-
+  printf("Enter a number\n");
+  scanf("%d",&n);
+  (n==0)?(n==0)?printf("Zero\n"):printf("Negative\n"):printf("Positve\n");*/
 }
 
