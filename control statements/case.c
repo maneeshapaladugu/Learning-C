774dc3b382988f88d101a90d1c05e75472e01990
maneeshapaladugu <41922387+maneeshapaladugu@users.noreply.github.com>
2018-08-08 22:36:18 +0530

    Add files via upload

---
+++ control statements/case.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+   char ch;
+   printf("Enter a character\n");
+   scanf("%c",&ch);
+   if(ch>='A'&&ch<='Z')
+      ch=ch|32;//to convert upper to lower case
+   else if(ch>='a'&&ch<='z')
+      ch=ch^32;//to convert lower to upper case
+   else
+      printf("Invalid character\n");
+   printf("Ch=%c\n",ch);
+}
