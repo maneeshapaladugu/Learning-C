1640d4782343531000ee0c953ebd7d6bce9d3a8f
maneeshapaladugu <41922387+maneeshapaladugu@users.noreply.github.com>
2018-08-08 22:27:52 +0530

    Add files via upload

---
+++ control statements/decimal_to_binary.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+ int n,i=sizeof(int)*8-1;
+ printf("Enter a number\n");
+ scanf("%d",&n);
+ binary:
+  if(n&(1<<i))
+   printf("1");
+  else
+   printf("0");
+  i--;
+ if(i>=0)
+ goto binary;
+}
+
