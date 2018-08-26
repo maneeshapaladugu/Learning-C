dfc61e1f3eb6d05f93979ccd042616836367eb14
maneeshapaladugu <41922387+maneeshapaladugu@users.noreply.github.com>
2018-08-26 16:33:10 +0530

    Add files via upload

---
+++ Loops/fibonacci.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int x=0,y=1,z=0,n;
+    printf("Enter range\n");
+    scanf("%d",&n);
+    printf("%d %d",x,y);
+    z=x+y;
+    while(z<=n)
+    {
+        printf(" %d",z);
+        x=y;
+        y=z;
+        z=x+y;
+    }
+    printf("\n");
+}
