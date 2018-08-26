147e7f629bcefec8108bea6517ed5a99ed6480cd
maneeshapaladugu <41922387+maneeshapaladugu@users.noreply.github.com>
2018-08-26 16:05:45 +0530

    Add files via upload

---
+++ Loops/while.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+   //To print binary equivalent using while 
+   /*int n,i=sizeof(int)*8-1;
+   printf("Enter a number\n");
+   scanf("%d",&n);
+   while(i>=0)
+   {
+      printf("%d",(n>>i)&1);
+      i--;
+   }*/
+   
+   //Using for loop
+   int n,i;
+   printf("Enter a number\n");
+   scanf("%d",&n);
+   for(i=sizeof(int)*8-1;i>=0;i--)
+       printf("%d",n>>i&1);
+   printf("\n"); 
+}
