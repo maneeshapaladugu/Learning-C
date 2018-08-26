ee521ba611f149586fc6498b53626bf65176f490
maneeshapaladugu <41922387+maneeshapaladugu@users.noreply.github.com>
2018-08-26 16:07:45 +0530

    Update and rename while.c to decimal_to_binary.c

--- Loops/decimal_to_binary.c
+++ Loops/decimal_to_binary.c
@@ -1,21 +1,21 @@
 #include<stdio.h>
 int main()
 {
-   //To print binary equivalent using while 
-   /*int n,i=sizeof(int)*8-1;
+   //To print binary equivalent using while loop 
+   int n,i=sizeof(int)*8-1;
    printf("Enter a number\n");
    scanf("%d",&n);
    while(i>=0)
    {
       printf("%d",(n>>i)&1);
       i--;
-   }*/
+   }
    
    //Using for loop
-   int n,i;
+   /*int n,i;
    printf("Enter a number\n");
    scanf("%d",&n);
    for(i=sizeof(int)*8-1;i>=0;i--)
        printf("%d",n>>i&1);
-   printf("\n"); 
+   printf("\n"); */
 }
