d3c4c0c9a75ce431f73e922efc5a5de550c3cfd0
maneeshapaladugu <41922387+maneeshapaladugu@users.noreply.github.com>
2018-08-27 22:45:19 +0530

    Add files via upload

---
+++ Basic/marks.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+  int m1,m2,m3,m4,m5,total;
+  float average,percentage;
+  printf("Enter marks in 5 subjects\n");
+  scanf("%d%d%d%d%d",&m1,&m2,&m3,&m4,&m5);
+  total=m1+m2+m3+m4+m5;
+  average=total/5;
+  percentage=((float)total/500)*100;
+  printf("Total=%d\n",total);
+  printf("The average of five subjects is %f\n",average);
+  printf("Percentage=%f%%\n",percentage);
+}
