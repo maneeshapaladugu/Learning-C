30c2819c3ca66cd99cea6be228a7ea5fe2c02118
maneeshapaladugu <41922387+maneeshapaladugu@users.noreply.github.com>
2018-08-27 17:17:56 +0530

    Add files via upload

---
+++ Arrays/big.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+   int a[5],n,i,small;
+   n=sizeof(a)/sizeof(a[0]);
+   printf("Enter elements\n");
+   for(i=0;i<n;i++)
+      scanf("%d",&a[i]);
+   small=a[0];
+   for(i=1;i<n;i++)
+   {
+       if(a[i]<small)
+          small=a[i];
+   }
+   printf("Smallest element is %d",small);
+   printf("\n");
+}
