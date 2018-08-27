ecd69c41a8f8e8a64af4b0ca7db05819732f2b09
maneeshapaladugu <41922387+maneeshapaladugu@users.noreply.github.com>
2018-08-27 23:07:29 +0530

    Add files via upload

---
+++ control statements/grade.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+  int m;
+  printf("Enter your mark\n");
+  scanf("%d",&m);
+  printf("Your grade is:\n"); 
+  if(m>=80)
+     printf("A grade\n");
+  else if(m>=70)
+     printf("B grade\n");
+  else if(m>=60)
+     printf("C grade\n");
+  else if(m>=50)
+     printf("D grade\n");
+  else if(m>=40)
+     printf("E grade\n");
+  else     
+     printf("Failed in the exam\n");
+}
