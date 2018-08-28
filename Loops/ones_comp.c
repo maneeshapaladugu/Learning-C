fa60ba20912dd9d9ff1ee55274920f66a35fe130
maneeshapaladugu <41922387+maneeshapaladugu@users.noreply.github.com>
2018-08-28 21:09:07 +0530

    Add files via upload

---
+++ Loops/ones_comp.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,ct,i;
+    printf("Enter a number\n");
+    scanf("%d",&n);
+    ct=~n;
+    printf("Binary of %d after complement:\n",n);
+    for(i=31;i>=0;i--)
+        printf("%d",ct>>i&1);
+    printf("\n");
+    printf("Decimal value of %d after complement:\n",n);
+    printf("%d\n",ct);
+    printf("Octal value of %d after complement:\n",n);
+    printf("%o\n",ct);
+    printf("Hexadecimal value of %d after complement:\n",n);
+    printf("%x\n",ct);
+}
