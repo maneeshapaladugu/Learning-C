8bbb74a2e1a0f361b97d25d463cf01d284ce3a5b
maneeshapaladugu <41922387+maneeshapaladugu@users.noreply.github.com>
2018-08-31 23:15:11 +0530

    Add files via upload

---
+++ Arrays/str_len.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+   //To find length of the string
+   char a[10];
+   int i;
+   printf("Enter a string\n");
+   scanf("%s",a);
+   for(i=0;a[i];i++);
+   printf("Length of the given string:\n%d\n",i);
+}
+ 
