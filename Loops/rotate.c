a3be78c86973a0ba3963b0f6c06110204cc7d759
maneeshapaladugu <41922387+maneeshapaladugu@users.noreply.github.com>
2018-09-02 07:59:13 +0530

    Update rotate.c

--- Loops/rotate.c
+++ Loops/rotate.c
@@ -2,8 +2,7 @@
 int main()
 {
  //To rotate bits of a given number
- unsigned int n;
- int r,msb,lsb,s=sizeof(int)*8-1,choice;
+ int n,r,choice;
  printf("Enter a number and no-of rotations\n");
  scanf("%d%d",&n,&r);
  printf("1. Left rotate\t2. Right rotate\n");
@@ -12,33 +11,23 @@ int main()
  switch(choice)
  {
    case 1: //Left rotate
-     printf("Before rotate\n");
-     for(int i=s;i>=0;i--)
+     printf("Before left rotate\n");
+     for(int i=31;i>=0;i--)
         printf("%d",(n>>i)&1);
-     for(int i=0;i<r;i++)
-     {
-        msb=(n>>s)&1;//store msb before shift on n
-        n=n<<1;//left shift n
-        n=n|msb;//add at lsb
-     }
-     printf("\nAfter rotate\n");
-     for(int i=s;i>=0;i--)
+     n=n>>32-r|n<<r;
+     printf("\nAfter left rotate\n");
+     for(int i=31;i>=0;i--)
         printf("%d",(n>>i)&1);
      printf("\n");
-        break;
+        break;33
 
   case 2: //right rotate
-     printf("Before rotate\n");
-     for(int i=s;i>=0;i--)
+     printf("Before right rotate\n");
+     for(int i=31;i>=0;i--)
         printf("%d",(n>>i)&1);
-     for(int i=0;i<r;i++)
-     {
-        lsb=(n&1)<<s;//store lsb before shift on n
-        n=n>>1;//right shift n
-        n=n|lsb;//add at msb
-     }
-     printf("\nAfter rotate\n");
-     for(int i=s;i>=0;i--)
+     n=n<<32-r|n>>r;
+     printf("\nAfter right rotate\n");
+     for(int i=31;i>=0;i--)
         printf("%d",(n>>i)&1);
      printf("\n");
         break;
