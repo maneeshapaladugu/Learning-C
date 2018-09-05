26a864e74dcc381fb28188aaae90d09b6996cb17
maneeshapaladugu <41922387+maneeshapaladugu@users.noreply.github.com>
2018-09-05 16:37:51 +0530

    Update nibble_swap.c

--- Loops/nibble_swap.c
+++ Loops/nibble_swap.c
@@ -1,15 +1,13 @@
 #include<stdio.h>
 int main()
 {
-  //To swap nibbles of a single byte
+  //To swap nibbles of a given byte
   int n,i,j;
   printf("Enter a number\n");
   scanf("%d",&n);
   printf("Before swapping nibbles\n");
   for(i=31;i>=0;i--)
-  {
-     printf("%d",n>>i&1);
-  }
+      printf("%d",n>>i&1);
   for(i=0,j=4;i<4;i++,j++)
   {
      if((n>>i&1)!=(n>>j&1))
@@ -18,11 +16,8 @@ int main()
          n=n^(1<<j);
      }
   }
-  printf("\n");
-  printf("After swapping nibbles\n");
+  printf("\nAfter swapping nibbles\n");
   for(i=31;i>=0;i--)
-  {
-     printf("%d",n>>i&1);
-  }
+      printf("%d",n>>i&1);
   printf("\n");
 }
