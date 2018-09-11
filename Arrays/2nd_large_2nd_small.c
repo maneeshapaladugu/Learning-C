8c5507afa264415eb671559ca6a69829ee34e588
maneeshapaladugu <41922387+maneeshapaladugu@users.noreply.github.com>
2018-09-11 15:34:16 +0530

    Update 2nd_large_2nd_small.c

--- Arrays/2nd_large_2nd_small.c
+++ Arrays/2nd_large_2nd_small.c
@@ -3,19 +3,19 @@ int main()
 {
    //To Find the 2nd largest and 2nd smallest elements of an unsorted array
    //Without using sorting technique
-   int a[10],i,n,big,big2,small,small2;
-   n=sizeof(a)/sizeof(a[0]);
+   int arr[10],i,n,big,big2,small,small2;
+   n=sizeof(arr)/sizeof(arr[0]);
    printf("Enter elements\n");
    for(i=0;i<n;i++)
-     scanf("%d",&a[i]);
-   big=a[0];
-   small=a[0];
+     scanf("%d",&arr[i]);
+   big=arr[0];
+   small=arr[0];
    for(i=0;i<n;i++)
    {
-      if(a[i]>big)
-         big=a[i];
-      if(a[i]<small)
-         small=a[i];
+      if(arr[i]>big)
+         big=arr[i];
+      if(arr[i]<small)
+         small=arr[i];
    }
    printf("Largest element in array:\n%d\n",big);
    printf("Smallest element in array:\n%d\n",small);
@@ -23,10 +23,10 @@ int main()
    small2=1000;
    for(i=0;i<n;i++)
    {
-      if(a[i]<big&&a[i]>big2)
-         big2=a[i];
-      if(a[i]>small&&a[i]<small2)
-         small2=a[i];
+      if(arr[i]<big&&arr[i]>big2)
+         big2=arr[i];
+      if(arr[i]>small&&arr[i]<small2)
+         small2=arr[i];
    }
  
    printf("Second largest element in array:\n%d\n",big2);
