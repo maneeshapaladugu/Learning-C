23ab49864ed6083339c219ffc51a58b0751f0f47
maneeshapaladugu <41922387+maneeshapaladugu@users.noreply.github.com>
2018-09-11 15:40:44 +0530

    Update del_element.c

--- Arrays/del_element.c
+++ Arrays/del_element.c
@@ -2,20 +2,20 @@
 int main()
 {
    //To delete element at desired position from an array 
-   int a[8],pos,n,i;
-   n=sizeof(a)/sizeof(a[0]);
+   int arr[8],pos,n,i;
+   n=sizeof(arr)/sizeof(arr[0]);
    printf("Enter elements\n");
    for(i=0;i<n;i++)
-      scanf("%d",&a[i]);
+      scanf("%d",&arr[i]);
    printf("Original array\n");
    for(i=0;i<n;i++)
-      printf("%d ",a[i]);
+      printf("%d ",arr[i]);
    printf("\nEnter position of element to be deleted\n");
    scanf("%d",&pos);
    for(i=pos;i<n;i++)
-      a[i]=a[i+1];      
+      arr[i]=arr[i+1];      
    printf("After deletion\n");
    for(i=0;i<n-1;i++)
-      printf("%d ",a[i]);
+      printf("%d ",arr[i]);
    printf("\n");
 }
