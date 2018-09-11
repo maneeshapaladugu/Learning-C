aafbee582e7fe8438442d655e2359a6c99d7ff93
maneeshapaladugu <41922387+maneeshapaladugu@users.noreply.github.com>
2018-09-11 15:42:00 +0530

    Update bubble_sort.c

--- Arrays/bubble_sort.c
+++ Arrays/bubble_sort.c
@@ -2,28 +2,28 @@
 int main()
 {
    //Sorting elements in ascending order using bubble sort method
-   int a[5],n,i,j,t;
-   n=sizeof(a)/sizeof(a[0]);
+   int arr[5],n,i,j,t;
+   n=sizeof(arr)/sizeof(arr[0]);
    printf("Enter elements\n");
    for(i=0;i<n;i++)
-       scanf("%d",&a[i]);
+       scanf("%d",&arr[i]);
    printf("Before sorting\n");
    for(i=0;i<n;i++)
-       printf("%d ",a[i]);
+       printf("%d ",arr[i]);
    for(i=1;i<n;i++)
    {
       for(j=0;j<n-i;j++)
       {
-           if(a[j]>a[j+1])
+           if(arr[j]>arr[j+1])
            {
-               t=a[j];
-               a[j]=a[j+1];
-               a[j+1]=t;
+               t=arr[j];
+               arr[j]=arr[j+1];
+               arr[j+1]=t;
            }
       }
    }
    printf("\nAfter sorting\n");
    for(i=0;i<n;i++)
-       printf("%d ",a[i]);
+       printf("%d ",arr[i]);
    printf("\n");
 }
