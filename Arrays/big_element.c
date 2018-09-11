d4a62c9bf4c306f7d589c4f48804f613ed5a8abb
maneeshapaladugu <41922387+maneeshapaladugu@users.noreply.github.com>
2018-09-11 15:37:12 +0530

    Update big_element.c

--- Arrays/big_element.c
+++ Arrays/big_element.c
@@ -2,16 +2,16 @@
 int main()
 { 
    //To find biggest element in an array
-   int a[5],n,i,big;
-   n=sizeof(a)/sizeof(a[0]);
+   int arr[5],n,i,big;
+   n=sizeof(arr)/sizeof(arr[0]);
    printf("Enter elements\n");
    for(i=0;i<n;i++)
-      scanf("%d",&a[i]);
-   big=a[0];
+      scanf("%d",&arr[i]);
+   big=arr[0];
    for(i=1;i<n;i++)
    {
-       if(a[i]>big)
-          big=a[i];
+       if(arr[i]>big)
+          big=arr[i];
    }
    printf("Biggest element is %d",big);
    printf("\n");
