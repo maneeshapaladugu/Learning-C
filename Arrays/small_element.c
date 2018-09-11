6ec0ffd20f5464c1dc6b978012a34bddb6620efc
maneeshapaladugu <41922387+maneeshapaladugu@users.noreply.github.com>
2018-09-11 15:48:01 +0530

    Update small_element.c

--- Arrays/small_element.c
+++ Arrays/small_element.c
@@ -2,16 +2,16 @@
 int main()
 {
    //To find smallest element in an array
-   int a[5],n,i,small;
-   n=sizeof(a)/sizeof(a[0]);
+   int arr[5],n,i,small;
+   n=sizeof(arr)/sizeof(arr[0]);
    printf("Enter elements\n");
    for(i=0;i<n;i++)
-      scanf("%d",&a[i]);
-   small=a[0];
+      scanf("%d",&arr[i]);
+   small=arr[0];
    for(i=1;i<n;i++)
    {
-       if(a[i]<small)
-          small=a[i];
+       if(arr[i]<small)
+          small=arr[i];
    }
    printf("Smallest element is %d",small);
    printf("\n");
