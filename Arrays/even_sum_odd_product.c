9fbc376a1bb047a6e1454e9d2ea9d81fd79db2b7
maneeshapaladugu <41922387+maneeshapaladugu@users.noreply.github.com>
2018-09-11 15:43:03 +0530

    Update even_sum_odd_product.c

--- Arrays/even_sum_odd_product.c
+++ Arrays/even_sum_odd_product.c
@@ -3,18 +3,18 @@ int main()
 {
    //Input 10 numbers through keyboard in to an array
    //Display results of addition of even numbers and product of odd numbers
-   int a[10],i,n,even=0,odd=1;
-   n=sizeof(a)/sizeof(a[0]);
+   int arr[10],i,n,even=0,odd=1;
+   n=sizeof(arr)/sizeof(arr[0]);
    printf("Enter elements\n");
    for(i=0;i<n;i++)
-     scanf("%d",&a[i]);
+     scanf("%d",&arr[i]);
    
    for(i=0;i<n;i++)
    {
-       if(a[i]%2==0)
-          even=even+a[i];
+       if(arr[i]%2==0)
+          even=even+arr[i];
        else
-          odd=odd*a[i];
+          odd=odd*arr[i];
    }
    printf("Addition of even numbers:\n%d\n",even);
    printf("product of odd numbers:\n%d\n",odd);
