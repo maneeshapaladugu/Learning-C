6315c31bf97c475b6856042b5ed81a3fb1828d32
maneeshapaladugu <41922387+maneeshapaladugu@users.noreply.github.com>
2018-09-11 15:47:17 +0530

    Update selection_sort.c

--- Arrays/selection_sort.c
+++ Arrays/selection_sort.c
@@ -2,29 +2,29 @@
 int main()
 {
    //Sorting elements in ascending order using selection sort method
-   int a[5],n,i,j,t;
-   n=sizeof(a)/sizeof(a[0]);
+   int arr[5],n,i,j,t;
+   n=sizeof(arr)/sizeof(arr[0]);
    printf("Enter elements\n");
    for(i=0;i<n;i++)
-      scanf("%d",&a[i]);
+      scanf("%d",&arr[i]);
    printf("Before sorting\n");
    for(i=0;i<n;i++)
-      printf("%d ",a[i]);
+      printf("%d ",arr[i]);
    for(i=0;i<n-1;i++)
    {
        for(j=i+1;j<n;j++)
        {
-          if(a[i]>a[j])
+          if(arr[i]>arr[j])
           {
-              t=a[i];
-              a[i]=a[j];
-              a[j]=t;
+              t=arr[i];
+              arr[i]=arr[j];
+              arr[j]=t;
           }
        }
    }
    printf("\nAfter sorting\n");
    for(i=0;i<n;i++)
-        printf("%d ",a[i]);
+        printf("%d ",arr[i]);
    printf("\n");
 }
    
