a18a66e680d7720b6ae5bf62f47afefe757f565a
maneeshapaladugu <41922387+maneeshapaladugu@users.noreply.github.com>
2018-10-10 15:13:54 +0530

    Add files via upload

---
+++ User defined functions/myatof.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+float my_atof(const char *p)
+{
+  int sum1=0,sum2=0,flag=0,factor=1;
+  while(*p)
+  {
+    if(*p=='.')
+    {
+       flag=1;
+       p++;
+    }
+    if(flag==0)
+     sum1=sum1*10+(*p-48);
+      
+    else if(flag==1)
+    {
+      factor*=10;
+      sum2=sum2*10+(*p-48);
+    }
+    p++;
+  
+  }
+  return sum1 + (float)sum2/factor;
+}
+int main()
+{
+  float x;
+  char a[10]="23.25";
+  x=my_atof(a);
+  printf("%f\n",x);
+}
+ 
+  
